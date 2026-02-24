f590464207b6a63b9880b850d1c42cbe525dc2eb
Stephanie Favero <dev.stephanie.favero@gmail.com>
2026-02-24 18:02:14 -0300

    Add files via upload

---
+++ projeto-smart-fila/projeto-smart-fila/projeto-smart-fila/projeto-smart-fila/backend/projeto-smart-fila/src/main/java/com/senai/smartfila/ProjetoSmartFilaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.smartfila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSmartFilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSmartFilaApplication.class, args);
+	}
+
+}
