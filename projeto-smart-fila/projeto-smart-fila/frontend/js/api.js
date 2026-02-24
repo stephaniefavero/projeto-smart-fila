ca4726cc7b30b35c54b31caf4ea7b8980d25d230
Stephanie Favero <dev.stephanie.favero@gmail.com>
2026-02-24 18:03:16 -0300

    Delete projeto-smart-fila/projeto-smart-fila directory

--- projeto-smart-fila/projeto-smart-fila/frontend/js/api.js
+++
@@ -1,3 +0,0 @@
-const URL_BASE = "http://localhost:8080/api/alunos";
-const form = document.querySelector('#alunoForm');
-const corpoTabela = document.querySelector('#corpoTabela');
--- projeto-smart-fila/projeto-smart-fila/frontend/js/deletarAluno.js
+++
@@ -1,6 +0,0 @@
-async function executarExclusao(id) {
-    if (confirm("Deseja excluir este aluno?")) {
-        await fetch(`${URL_BASE}/${id}`, { method: 'DELETE' });
-        listarAlunos();
-    }
-}
